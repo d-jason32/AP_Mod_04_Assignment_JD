34864b46d0d00a62f1119d713a840ac0df18a35b
Jason Devaraj <jdevaraj01@gmail.com>
2025-03-07 22:00:14 -0500

    Working on refactor.

--- src/main/java/edu/farmingdale/Day.java
+++ src/main/java/edu/farmingdale/Day.java
@@ -1,5 +1,76 @@
 package edu.farmingdale;
 
-public record Day(String date, double temperature, int humidity, double precipitation) {
+import java.util.ArrayList;
 
+public record Day(String date, double temperature, int humidity, double precipitation) {
+    public static ArrayList<Day> parseFile(ArrayList<String> readFile) {
+        ArrayList<Day> parsedFile = new ArrayList<>();
+
+        readFile.forEach((n) -> {
+            String[] parts = n.split(",");
+
+            String date = parts[0];
+            double temperature = Double.parseDouble(parts[1]);
+            int humidity = Integer.parseInt(parts[2]);
+            double precipitation = Double.parseDouble(parts[3]);
+
+            Day newDay = new Day(date, temperature, humidity, precipitation);
+
+            parsedFile.add(newDay);
+        });
+        return parsedFile;
+    }
+
+    public static ArrayList<String> returnCategories(ArrayList<Day> dayList){
+        ArrayList<String> list = new ArrayList<>();
+        dayList.forEach((day)-> {
+
+            String s = switch (day){
+                case Day d when day.temperature > 50  -> day.date + " Category: " + "hot";
+                case Day d when day.temperature > 30 -> day.date + " Category: " + "warm";
+                case Day d when day.temperature >= 0 -> day.date + " Category: " + "cold";
+                default -> "Error";
+            };
+            list.add(s);
+        });
+        return list;
+    }
+
+    public static int numRainyDays(ArrayList<Day> dayList){
+        int numRainyDays = (int) dayList.stream()
+                .filter(day -> day.precipitation > 0)
+                .count();
+
+        return numRainyDays;
+    }
+
+    public static int daysOver50(ArrayList<Day> dayList){
+        long daysOver50 =
+                dayList.stream()
+                        .filter(day -> day.temperature > 50)
+                        .count();
+
+        return (int) daysOver50;
+    }
+
+    public static double getAverageTemperature(ArrayList<Day> dayList){
+        double total = dayList.stream()
+                .mapToDouble(day -> day.temperature)
+                .sum();
+
+        return total / dayList.size();
+    }
+
+
+    public static String getTextBlock(double averageTemp, long daysOver50, long numRainyDays, ArrayList<String> categories){
+        String list = String.join("\n", categories);
+
+        return """
+                Average Temperature of the Month: %.2f
+                Number of days over 50 degrees: %d
+                Number of rainy days: %d
+                Categories list:
+                %s
+               """.formatted(averageTemp, daysOver50, numRainyDays, list);
+    }
 }
--- src/main/java/edu/farmingdale/Main.java
+++ src/main/java/edu/farmingdale/Main.java
@@ -6,87 +6,41 @@ import java.io.FileReader;
 
 
 public class Main {
-    public static void main(String[] args) {
-        ArrayList<String> dataList = new ArrayList<>();
-        ArrayList<Day> dayList = new ArrayList<>();
 
-        /*
-        Try-catch statement that opens and reads weatherdata.csv.
-        Creates player objects and populates pqList1 with player objects.
-         */
+    public static void main(String[] args) {
+        ArrayList<String> readFile = readFile("weatherdata.csv");
+        ArrayList<Day> parsedFile = Day.parseFile(readFile);
+
+        int numRainyDays = Day.numRainyDays(parsedFile);
+        int daysOver50 = Day.daysOver50(parsedFile);
+        double averageTemp = Day.getAverageTemperature(parsedFile);
+        ArrayList<String> categories = Day.returnCategories(parsedFile);
+        String textBlock = Day.getTextBlock(averageTemp, daysOver50, numRainyDays, categories);
+
+        System.out.println(textBlock);
+    }
+
+    public static ArrayList<String> readFile(String fileName){
+        ArrayList<String> dataList = new ArrayList<>();
+
         try {
-            FileReader fr = new FileReader("weatherdata.csv");
+            FileReader fr = new FileReader(fileName);
             Scanner infile = new Scanner(fr);
             String data;
-
             while (infile.hasNext()) {
+
                 data = infile.nextLine();
 
                 dataList.add(data);
-
             }
         } catch (FileNotFoundException e) {
+
             throw new RuntimeException(e);
         }
 
+        // Remove first line.
         dataList.remove(0);
-
-        dataList.forEach( (n) -> {
-            String[] parts = n.split(",");
-
-            String date = parts[0];
-            double temperature = Double.parseDouble(parts[1]);
-            int humidity = Integer.parseInt(parts[2]);
-            double precipitation = Double.parseDouble(parts[3]);
-
-            Day newDay = new Day(date, temperature, humidity, precipitation);
-
-            dayList.add(newDay);
-        });
-
-
-
-        double total = dayList.stream()
-                .mapToDouble(day -> day.temperature)
-                .sum();
-
-        double averageTemp = total / dayList.size();
-
-
-
-        long daysOver50 =
-                dayList.stream()
-                .filter(day -> day.temperature > 50)
-                .count();
-
-
-        long numRainyDays = dayList.stream()
-                .filter(day -> day.precipitation > 0)
-                .count();
-
-        System.out.println(getTextBlock(averageTemp, daysOver50, numRainyDays));
-
-        dayList.forEach((day)-> {
-
-            String s = switch (day){
-                case Day d when day.temperature > 50  -> day.date + " Category: " + "hot";
-                case Day d when day.temperature > 30 -> day.date + " Category: " + "warm";
-                case Day d when day.temperature >= 0 -> day.date + " Category: " + "cold";
-                default -> "Error";
-            };
-
-            System.out.println(s);
-        });
-    }
-
-
-
-    public static String getTextBlock(double averageTemp, long daysOver50, long numRainyDays){
-        return """
-                Average Temperature of the Month: %.2f
-                Number of days over 50 degrees: %d
-                Number of rainy days: %d
-               """.formatted(averageTemp, daysOver50, numRainyDays);
+        return dataList;
     }
 
 }
