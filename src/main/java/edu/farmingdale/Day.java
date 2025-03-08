b63cd8a125b5a8a28d5993c622292c8ac3cc39ff
Jason Devaraj <jdevaraj01@gmail.com>
2025-03-07 22:32:57 -0500

    Add comments.

--- src/main/java/edu/farmingdale/Day.java
+++ src/main/java/edu/farmingdale/Day.java
@@ -2,11 +2,26 @@ package edu.farmingdale;
 
 import java.util.ArrayList;
 
+/**
+ *
+ * @author Jason Devaraj
+ * @param date
+ * @param temperature
+ * @param humidity
+ * @param precipitation
+ */
 public record Day(String date, double temperature, int humidity, double precipitation) {
+
+    /**
+     *
+     * @param readFile
+     * @return
+     */
     public static ArrayList<Day> parseFile(ArrayList<String> readFile) {
         ArrayList<Day> parsedFile = new ArrayList<>();
 
         readFile.forEach((n) -> {
+            // Put each comma separated value into a list.
             String[] parts = n.split(",");
 
             String date = parts[0];
@@ -14,6 +29,7 @@ public record Day(String date, double temperature, int humidity, double precipit
             int humidity = Integer.parseInt(parts[2]);
             double precipitation = Double.parseDouble(parts[3]);
 
+            // Create new Day record with the parsed data.
             Day newDay = new Day(date, temperature, humidity, precipitation);
 
             parsedFile.add(newDay);
@@ -21,13 +37,21 @@ public record Day(String date, double temperature, int humidity, double precipit
         return parsedFile;
     }
 
+    /**
+     *
+     * @param dayList
+     * @return
+     */
     public static ArrayList<String> returnCategories(ArrayList<Day> dayList){
         ArrayList<String> list = new ArrayList<>();
-        dayList.forEach((day)-> {
 
+        dayList.forEach((day)-> {
             String s = switch (day){
+                // When temperature is above 50, day is categorized by hot.
                 case Day d when day.temperature > 50  -> day.date + " Category: " + "hot";
+                // When temperature is above 30, day is categorized by warm.
                 case Day d when day.temperature > 30 -> day.date + " Category: " + "warm";
+                // When temperature is above or equal to0, day is categorized by cold.
                 case Day d when day.temperature >= 0 -> day.date + " Category: " + "cold";
                 default -> "Error";
             };
@@ -36,23 +60,39 @@ public record Day(String date, double temperature, int humidity, double precipit
         return list;
     }
 
+    /**
+     *
+     * @param dayList
+     * @return
+     */
     public static int numRainyDays(ArrayList<Day> dayList){
-        int numRainyDays = (int) dayList.stream()
+        long numRainy =  dayList.stream()
+                // Filter for days with more than 0 inches of precipitation.
                 .filter(day -> day.precipitation > 0)
                 .count();
 
-        return numRainyDays;
+        return (int) numRainy;
     }
 
+    /**
+     *
+     * @param dayList
+     * @return
+     */
     public static int daysOver50(ArrayList<Day> dayList){
-        long daysOver50 =
-                dayList.stream()
+        long daysOver50 = dayList.stream()
+                        // Filter for days that have a temperature greater than 50.
                         .filter(day -> day.temperature > 50)
                         .count();
 
         return (int) daysOver50;
     }
 
+    /**
+     *
+     * @param dayList
+     * @return
+     */
     public static double getAverageTemperature(ArrayList<Day> dayList){
         double total = dayList.stream()
                 .mapToDouble(day -> day.temperature)
@@ -61,16 +101,25 @@ public record Day(String date, double temperature, int humidity, double precipit
         return total / dayList.size();
     }
 
-
+    /**
+     *
+     * @param averageTemp
+     * @param daysOver50
+     * @param numRainyDays
+     * @param categories
+     * @return
+     */
     public static String getTextBlock(double averageTemp, long daysOver50, long numRainyDays, ArrayList<String> categories){
+        // Combine elements of the list into a string with each elements
+        // separated by a new line.
         String list = String.join("\n", categories);
 
         return """
-                Average Temperature of the Month: %.2f
-                Number of days over 50 degrees: %d
-                Number of rainy days: %d
-                Categories list:
-                %s
+               Average Temperature of the Month: %.2f
+               Number of days over 50 degrees: %d
+               Number of rainy days: %d
+               Categories list:
+               %s
                """.formatted(averageTemp, daysOver50, numRainyDays, list);
     }
 }
--- src/main/java/edu/farmingdale/Main.java
+++ src/main/java/edu/farmingdale/Main.java
@@ -4,24 +4,36 @@ import java.util.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-
+/**
+ *
+ * @author Jason Devaraj
+ */
 public class Main {
-
     public static void main(String[] args) {
         // Generate list from csv file.
         ArrayList<String> readFile = readFile("weatherdata.csv");
-        //
+        // Create list of Day records.
         ArrayList<Day> parsedFile = Day.parseFile(readFile);
 
+        // Calculate number of rainy days.
         int numRainyDays = Day.numRainyDays(parsedFile);
+        // Calculate number of days over 50 degrees.
         int daysOver50 = Day.daysOver50(parsedFile);
+        // Calculate the average temperature of the month.
         double averageTemp = Day.getAverageTemperature(parsedFile);
+        // Categorize each day.
         ArrayList<String> categories = Day.returnCategories(parsedFile);
+        // Generate text block of the data.
         String textBlock = Day.getTextBlock(averageTemp, daysOver50, numRainyDays, categories);
 
         System.out.println(textBlock);
     }
 
+    /**
+     *
+     * @param fileName
+     * @return
+     */
     public static ArrayList<String> readFile(String fileName){
         ArrayList<String> dataList = new ArrayList<>();
 
@@ -30,24 +42,18 @@ public class Main {
             Scanner infile = new Scanner(fr);
             String data;
             while (infile.hasNext()) {
-
                 data = infile.nextLine();
-
                 dataList.add(data);
             }
         } catch (FileNotFoundException e) {
-
             throw new RuntimeException(e);
         }
-
         // Remove first line.
-        dataList.remove(0);
+        dataList.removeFirst();
         return dataList;
     }
-
 }
 
-// Refactor
 // readme file
 // markdown support in javadoc. Add javadoc comments. // include code examples
-// pattern matching for switch
+
